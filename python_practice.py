86b4c4c7f06632160c31edeaf70b85c679e43cbc
samsonAz64 <101012682+samsonAz64@users.noreply.github.com>
2022-04-23 11:21:18 -0400

    Add files via upload

---
+++ python_practice.py
@@ -0,0 +1,44 @@
+countries = ["Arapahoe", "denzer","jefferson" ]
+print(countries)
+print(len(countries))
+countries.append("samson")
+print(countries)
+countries.index("samson")
+print(countries)
+countries[3]= "Nigeria"
+print(countries)
+countries.pop(0)
+print(countries)
+print(len(countries))
+
+#list of actors
+actors=["tom cruisse",'samson az']
+print(actors)
+actors = {"name": "samson",
+"genre": "action",
+"age": 25}
+print(actors["age"])
+
+temperature = int(input("what is the temperature outside?"))
+if temperature > 80:
+    print("turn on AC.")
+else:
+        print("open the Windows")
+#what is the score 
+score = int(input("what is your test score?"))
+if score >= 90:
+  80
+  print("your grade is an A.")
+else:
+   if score >=80:
+        print("print your grade is B")
+
+        x = 0
+        
+        while x <= 5: 
+           print(x)
+           x = x+1
+           numbers = [0,1,2,3,4,5]
+           for num in numbers:
+            print(num)
+
